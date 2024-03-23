ac0348952bd964d2e6d27a24211e8b36f15788a8
GrekaSan <Greka-San@yandex.ru>
2024-03-23 18:40:04 +0300

    done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+example = Item("Телевизор", 30_000, 10)
+
+
+def test_calculate_total_price():
+    assert example.calculate_total_price() == 300_000
+
+
+def test_apply_discount():
+    example.pay_rate = 0.5
+    example.apply_discount()
+    assert example.price == 15_000
