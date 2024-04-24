9c44f421435aee67c8573b7f4157e0f0b1eea5ff
GrekaSan <Greka-San@yandex.ru>
2024-04-24 23:16:14 +0300

    home work 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -31,3 +34,34 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(self.__name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, csvfile):
+        Item.all.clear()
+        with open(csvfile, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(str(row['name']), float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(number_string):
+        for number in number_string:
+            if number.isdigit():
+                return int(number)
+
+
+# csvfile = '../src/items.csv'
+# Item.instantiate_from_csv(csvfile)
+# print(Item.all)
+
